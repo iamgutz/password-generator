3a7cebaa71d564f47a2c978f3e7a434f1e91f50e
Michael Gutierrez <michael@qodeware.co>
2024-01-03 11:15:14 -0600

    improved generatePassword function with edge cases handling

--- src/utils/generate-password.ts
+++ src/utils/generate-password.ts
@@ -11,6 +11,19 @@ const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
 const NUMBERS = '0123456789';
 const SYMBOLS = '!@#$%^&*()-_=+[]{}|;:,.<>?/';
 
+const getRandomChar = (charset: string): string => {
+  const randomIndex = Math.floor(Math.random() * charset.length);
+  return charset[randomIndex] || '';
+};
+
+const shufflePassword = (password: string): string => {
+  // Convert the password string to an array, shuffle, and then join back into a string
+  return password
+    .split('')
+    .sort(() => Math.random() - 0.5)
+    .join('');
+};
+
 const generatePassword = ({
   length,
   uppercase = true,
@@ -23,27 +36,36 @@ const generatePassword = ({
 
   if (uppercase) {
     charset += UPPERCASE;
+    // make sure it contains at least 1 uppercase
+    password += getRandomChar(UPPERCASE);
   }
 
   if (lowercase) {
     charset += LOWERCASE;
+    // make sure it contains at least 1 lowercase
+    password += getRandomChar(LOWERCASE);
   }
 
   if (numbers) {
     charset += NUMBERS;
+    // make sure it contains at least 1 number
+    password += getRandomChar(NUMBERS);
   }
 
   if (symbols) {
     charset += SYMBOLS;
+    // make sure it contains at least 1 symbol
+    password += getRandomChar(SYMBOLS);
   }
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    const character = charset[randomIndex];
-    password += character || '';
+  // Fill the remaining length randomly from the combined charset
+  for (let i = password.length; i < length; i++) {
+    password += getRandomChar(charset);
   }
 
-  return password;
+  // return a shuffled password to avoid having same pattern of initial chars
+  // trim the password to the desired length
+  return shufflePassword(password).slice(0, length);
 };
 
 export default generatePassword;
