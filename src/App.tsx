868beb279663544476ab135cd5109785da7e3920
Michael Gutierrez <michael@qodeware.co>
2024-01-03 13:09:54 -0600

    added functionality to generate new password every time there's a change in the options

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Paper from './components/Paper';
+import generatePassword from './utils/generate-password';
 
 function App() {
   const [passwordLength, setPasswordLength] = useState('12');
+  const [options, setOptions] = useState({
+    uppercase: true,
+    lowercase: true,
+    numbers: true,
+    symbols: true,
+  });
+  const initialPassword = generatePassword({
+    length: parseInt(passwordLength, 10),
+    ...options,
+  });
+  const [password, setPassword] = useState(initialPassword);
+
   const handlePasswordLengthChange = (length: string) => {
     setPasswordLength(length);
   };
+
+  const handleOptionsChange = (optionName: string, value: boolean) => {
+    setOptions(prevOptions => ({
+      ...prevOptions,
+      [optionName]: value,
+    }));
+  };
+
+  useEffect(() => {
+    const newPassword = generatePassword({ length: parseInt(passwordLength, 10), ...options });
+    setPassword(newPassword);
+  }, [passwordLength, options]);
+
   return (
     <div className="bg-slate-800 min-h-screen flex flex-col items-center justify-center">
       <Paper>
@@ -23,10 +49,39 @@ function App() {
           />
         </div>
 
+        <div>
+          <p>Include:</p>
+          <label>Uppercase</label>
+          <input
+            type="checkbox"
+            checked={options.uppercase}
+            onChange={e => handleOptionsChange('uppercase', e.target.checked)}
+          />
+          <label>Lowercase</label>
+          <input
+            type="checkbox"
+            checked={options.lowercase}
+            onChange={e => handleOptionsChange('lowercase', e.target.checked)}
+          />
+          <label>Numbers</label>
+          <input
+            type="checkbox"
+            checked={options.numbers}
+            onChange={e => handleOptionsChange('numbers', e.target.checked)}
+          />
+          <label>Symbols</label>
+          <input
+            type="checkbox"
+            checked={options.symbols}
+            onChange={e => handleOptionsChange('symbols', e.target.checked)}
+          />
+        </div>
+
         <div className="my-3">
           <input
             type="text"
             className="text-black"
+            value={password}
           />
           <button type="button">Copy</button>
         </div>
